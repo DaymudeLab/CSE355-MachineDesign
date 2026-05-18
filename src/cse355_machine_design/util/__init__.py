533dafe2caef02c0d21bdf486a243d0c68c557b1
Josh Daymude <jdaymude@asu.edu>
2026-05-18 12:20:48 -0700

    refactor: Document and cleanup the utils functions

--- src/cse355_machine_design/util/__init__.py
+++ src/cse355_machine_design/util/__init__.py
@@ -1,12 +1,22 @@
-from typing import Set
+def quote_str(s: str) -> str:
+    """
+    Return the given string in quotes.
+
+    :param s: An input string to wrap in quotes.
+    :return: The input string wrapped in quotes.
+    """
+    return f'"{s}"'
 
 
-def quote_str(string: str) -> str:
-    return f'"{string}"'
+def set_str(s: set[str]) -> str:
+    """
+    Return a string representation of a Set, specifically designed to use "{}"
+    for an empty set instead of "set()".
 
-
-def set_str(input: Set[str]) -> str:
-    if len(input) == 0:
+    :param s: A Set to represent as a string.
+    :return: The string representation of the input Set.
+    """
+    if len(s) == 0:
         return "{}"
     else:
-        return str(input)
+        return str(s)
