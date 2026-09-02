9cd736e5847cef3797eb6cb8f959e441e0027846
Josh Daymude <jdaymude@asu.edu>
2026-09-01 23:50:57 -0700

    refactor: Clean up and document the base automata class

--- src/cse355_machine_design/automata/base.py
+++ src/cse355_machine_design/automata/base.py
@@ -1,95 +1,88 @@
+from cse355_machine_design import registry
+
 from abc import ABC, abstractmethod
-from typing import Set, Union
+from pathlib import Path
 
-# from PIL import Image, ImageOps
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import urllib.request
-# from urllib.parse import quote
-# import io
 import render_html
-import pathlib
 
 
-class _Automata(ABC):
-    _states: Set[str]
-    _input_symbols: Set[str]
-    _start_state: str
+# Define a type alias for automata states (to avoid confusion with usual strs).
+type State = str
+
+
+class _Automaton(ABC):
+    """
+    An abstract base class for automata.
+    """
+
+    # Automata variables; note that the transition function is not included
+    # here because it must be defined by a specific derived class.
+    _type: str  # Type of automata (DFA, NFA, PDA, etc.).
+    _states: set[State]  # State set Q.
+    _alphabet: set[str]  # Alphabet Sigma.
+    _start_state: State  # Start state q0.
+    _accept_states: set[State]  # Accept states F.
 
     def __init__(self) -> None:
-        self.verify_legality()
+        """
+        Create a new automaton and then validate it.
+        """
+        self.validate()
 
     @abstractmethod
-    def verify_legality(self) -> None:
+    def validate(self) -> None:
         """
-        Verify the basic n-tuple requirements
+        Validate this automaton according to its formal definition.
         """
         raise NotImplementedError("Abstract method not callable")
 
     @abstractmethod
-    def evaluate(self, input_str: str, enable_trace=0) -> Union[bool, None]:
+    def evaluate(self, input_str: str, trace: bool = False) -> bool:
         """
-        Evaluate the automata on a string
+        Evaluate this automaton on the given input string.
+
+        :param input_str: An input string to evaluate.
+        :param trace: True iff tracing information should be printed.
+        :return: True iff the automaton accepts the input string.
+        """
+        raise NotImplementedError("Abstract method not callable")
+
+    def submit_as_answer(self, problem_number: int) -> None:
+        """
+        Submit this automaton to the registry as the answer to a given problem.
+        Overwrites any previously submitted automaton for the same problem.
+
+        :param problem_number: The problem to submit this as an answer for.
+        """
+        registry.add_to_registry(self._type, problem_number, self)
+
+    @abstractmethod
+    def as_dict(self) -> dict:
+        """
+        Get a dict representation of this automaton.
+
+        :return: A dict representation of this automaton.
         """
         raise NotImplementedError("Abstract method not callable")
 
     @abstractmethod
-    def submit_as_answer(self, question_number: int) -> None:
+    def _as_dot_string(self) -> str:
         """
-        Submit this automata as the answer to a given question.
+        Get a DOT string representation of this automaton for use in graphviz
+        visualization.
 
-        #### Enter the question number exactly as seen on the homework assignment.
-
-        This overrides any prior calls to this function for the given question.
-        """
-        raise NotImplementedError("Abstract method not callable")
-
-    @abstractmethod
-    def export_to_dict(self) -> dict:
-        """
-        In the following Python Dict spec, describe the machine:
-        {
-            "type": "dfa",
-            "states": str[],
-            "input_symbols": str[]
-            "delta": {
-                        "from": str,
-                        "to": str,
-                        "input": str[] //<-- singleton array
-                    }[]
-            "start_state": str,
-            "finals": str[]
-        } |
-        {
-            "type": "nfa",
-            "states": str[],
-            "input_symbols": str[]
-            "delta": {
-                        "from": str,
-                        "to": str[],
-                        "input": str
-                    }[]
-            "start_state": str,
-            "finals": str[],
-            "epsilon_char": str
-        }
-        """
-        raise NotImplementedError("Abstract method not callable")
-
-    @abstractmethod
-    def _generate_dot_string(self) -> str:
-        """
-        Display the machine state diagram as a graphviz plot
+        :return: A DOT string representation of this automaton.
         """
         raise NotImplementedError("Abstract method not callable")
 
     def display_state_diagram(self) -> None:
         print("Opening state diagram in default browser...")
-        html_contents = (
-            '<!doctypehtml><title>Automata Visualization</title><meta content="View your automata in the browser"name=description><script src=https://cdn.jsdelivr.net/npm/@viz-js/viz@3.2.4/lib/viz-standalone.min.js></script><script>Viz.instance().then(function(e){var n=e.renderSVGElement(\''
-            + self._generate_dot_string()
-            + '\');document.getElementById("graph").appendChild(n)})</script><div id=graph></div>'
-        )
-        render_html.render_in_browser(
-            html_contents, str(pathlib.Path.cwd() / "preview.html")
-        )
+        html_contents = f"""\
+            <!doctypehtml>
+            <title>Automaton Visualization</title>
+            <meta content="View your automaton in the browser"name=description>
+            <script src=https://cdn.jsdelivr.net/npm/@viz-js/viz@3.2.4/lib/viz-standalone.min.js></script>
+            <script>Viz.instance().then(function(e){{var n=e.renderSVGElement('{self._as_dot_string()}');document.getElementById("graph").appendChild(n)}})</script>
+            <div id=graph></div>\
+        """
+        render_html.render_in_browser(html_contents, str(Path.cwd() / "preview.html"))
