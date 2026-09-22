943fffdee313eede6bd1343f4a432e14a3216296
Josh Daymude <jdaymude@asu.edu>
2026-09-22 14:38:10 -0700

    feat: Implement CFG normalization based on Hopcroft-Motwani-Ullman

--- src/cse355_machine_design/automata/__init__.py
+++ src/cse355_machine_design/automata/__init__.py
@@ -1,4 +1,5 @@
 from .dfa import _DFA as DFA
 from .nfa import _NFA as NFA
 from .pda import _PDA as PDA
+from .cfg import _CFG as CFG
 from .bbtm import _BusyBeaverTM as BBTM
--- src/cse355_machine_design/automata/cfg.py
+++ src/cse355_machine_design/automata/cfg.py
@@ -1,5 +1,8 @@
 from cse355_machine_design.errors import DetailedError
 
+from collections import defaultdict
+from itertools import chain, combinations
+
 from typeguard import typechecked
 
 
@@ -99,8 +102,8 @@ class _CFG:
         # Each rule should have a single variable on its left-hand side and one
         # or more variables or terminals on its right-hand side.
         err = ""
-        for lhs, rhss in self._rules.items():
-            for rhs in rhss:
+        for lhs in self._rules:
+            for rhs in self._rules[lhs]:
                 r_err = ""
                 if lhs not in self._variables:
                     r_err += (
@@ -131,3 +134,139 @@ class _CFG:
                 f"The start variable '{self._start_variable}' does not appear "
                 + "on the left-hand side of any rule.",
             )
+
+    def normalize(self) -> None:
+        """
+        Normalize this CFG by removing, in this order:
+        - rules of the form A -> epsilon, except possibly S -> epsilon
+        - unit rules of the form A -> B (and, by extension, cycles A =>* A)
+        - unproductive variables, or variables A with no derivation A =>* w,
+          where w is a string of terminals
+        - unreachable variables, or variables A with no derivation S =>* xAy,
+          where S is the start variable and x and y are possibly empty strings
+          of variables and terminals
+
+        Details of these procedures can be found in Hopcroft, Motwani, and
+        Ullman (3rd ed., 2006), Chapter 7.1.
+        """
+
+        # Strip unnecessary epsilon terminals.
+        def strip_epsilon(rhs: tuple[str, ...]) -> tuple[str, ...]:
+            if self._epsilon not in rhs or len(rhs) == 1:
+                return rhs
+            else:
+                return tuple([x for x in rhs if x != self._epsilon])
+
+        for lhs in self._rules:
+            self._rules[lhs] = {strip_epsilon(rhs) for rhs in self._rules[lhs]}
+
+        # Identify all "nullable" variables A such that A =>* epsilon.
+        nullable: set[str] = {self._epsilon}
+        added_new_nullable = True
+        while added_new_nullable:
+            added_new_nullable = False
+            for lhs in self._rules.keys() - nullable:
+                for rhs in self._rules[lhs]:
+                    if set(rhs) <= nullable:
+                        nullable.add(lhs)
+                        added_new_nullable = True
+                        break
+
+        # Remove epsilon rules, except possibly S -> epsilon.
+        nonepsilon_rules: dict[str, set[tuple[str, ...]]] = defaultdict(set)
+        if tuple(self._epsilon) in self._rules[self._start_variable]:
+            nonepsilon_rules[self._start_variable].add(tuple(self._epsilon))
+        for lhs in self._rules:
+            for rhs in self._rules[lhs]:
+                # For each possible subset of the nullable variables in this
+                # rule's right-hand side, create a new right-hand side with
+                # those variables as null. Drop totally null right-hand sides.
+                nullable_ixs: set[int] = {
+                    ix for (ix, s) in enumerate(rhs) if s in nullable
+                }
+                for nulled_ixs in chain.from_iterable(
+                    combinations(nullable_ixs, r) for r in range(len(nullable_ixs) + 1)
+                ):
+                    nulled_rhs = tuple(
+                        s for (ix, s) in enumerate(rhs) if ix not in nulled_ixs
+                    )
+                    if len(nulled_rhs) > 0:
+                        nonepsilon_rules[lhs].add(nulled_rhs)
+        self._rules = dict(nonepsilon_rules)
+
+        # Identify "unit" rules of the form A -> B where B is a variable.
+        unit_rules: dict[str, set[tuple[str, ...]]] = defaultdict(set)
+        for lhs in self._rules:
+            for rhs in self._rules[lhs]:
+                if len(rhs) == 1 and rhs[0] in self._variables:
+                    unit_rules[lhs].add(rhs)
+        unit_rules = dict(unit_rules)
+
+        # Identify all "unit pairs" of variables (A, B) such that A =>* B using
+        # only unit rules. In the base case, A =>* A using zero rules.
+        unit_pairs: set[tuple[str, str]] = {(A, A) for A in self._variables}
+        unit_pairs_to_explore = unit_pairs.copy()
+        while len(unit_pairs_to_explore) > 0:
+            # In the inductive case, if (A, B) is a unit pair and B -> C is a
+            # unit rule, then (A, C) is a unit pair.
+            new_unit_pairs: set[tuple[str, str]] = set()
+            for A, B in unit_pairs_to_explore:
+                if B in unit_rules:
+                    new_unit_pairs |= {(A, rhs[0]) for rhs in unit_rules[B]}
+
+            unit_pairs_to_explore = new_unit_pairs - unit_pairs
+            unit_pairs |= new_unit_pairs
+
+        # Remove unit rules: for each unit pair (A, B), if B -> alpha is a
+        # non-unit rule, then include A -> alpha.
+        nonunit_rules: dict[str, set[tuple[str, ...]]] = defaultdict(set)
+        for A, B in unit_pairs:
+            nonunit_rules[A] |= {rhs for rhs in self._rules[B] - unit_rules[B]}
+        self._rules = dict(nonunit_rules)
+
+        # Identify all productive elements (terminals and variables). In the
+        # base case, terminal symbols and epsilon are self-producing.
+        productive_elements: set[str] = self._terminals | {self._epsilon}
+        while True:
+            # In the inductive case, if A -> alpha is a rule and every element
+            # of alpha is productive, then A is productive.
+            new_productive_elements: set[str] = set()
+            for lhs in self._rules.keys() - productive_elements:
+                for rhs in self._rules[lhs]:
+                    if set(rhs) <= productive_elements:
+                        new_productive_elements.add(lhs)
+                        break
+
+            if len(new_productive_elements) == 0:
+                break
+            else:
+                productive_elements |= new_productive_elements
+
+        # Remove unproductive variables and rules that contain them.
+        unproductive_variables = self._variables - productive_elements
+        self._variables &= productive_elements
+        for lhs in self._rules.keys() & unproductive_variables:
+            del self._rules[lhs]
+        for lhs in self._rules:
+            self._rules[lhs] = {
+                rhs
+                for rhs in self._rules[lhs]
+                if len(set(rhs) & unproductive_variables) == 0
+            }
+
+        # Identify all reachable variables, beginning with the start variable.
+        reachable_variables: set[str] = {self._start_variable}
+        variables_to_explore: set[str] = {self._start_variable}
+        variables_explored: set[str] = set()
+        while len(variables_to_explore) > 0:
+            lhs = variables_to_explore.pop()
+            variables_explored.add(lhs)
+            for rhs in self._rules[lhs]:
+                rhs_variables = set(rhs) & self._variables
+                reachable_variables |= rhs_variables
+                variables_to_explore |= rhs_variables - variables_explored
+
+        # Drop unreachable variables and rules that contain them.
+        self._variables = reachable_variables
+        for lhs in self._rules.keys() - reachable_variables:
+            del self._rules[lhs]
