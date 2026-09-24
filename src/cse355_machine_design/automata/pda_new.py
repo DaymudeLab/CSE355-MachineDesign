63a9c4e52d805ea91c655a7557641f966e59348c
Josh Daymude <jdaymude@asu.edu>
2026-09-24 01:06:48 -0700

    refactor: Reimplement PDA to CFG conversion and use it to eval strings

--- src/cse355_machine_design/automata/pda_new.py
+++ src/cse355_machine_design/automata/pda_new.py
@@ -1,7 +1,9 @@
 from cse355_machine_design.automata.base import _Automaton, State
+from cse355_machine_design.automata import CFG
 from cse355_machine_design.errors import DetailedError
 
 from collections import defaultdict
+from itertools import product
 
 from typeguard import typechecked
 
@@ -146,7 +148,8 @@ class _PDA(_Automaton):
                 + "alphabet.",
             )
 
-        # TODO: Complete.
+        # TODO: Honor tracing parameter.
+        return self._as_cfg().generates_string(input_str)
 
     def as_dict(self) -> dict:
         """
@@ -212,3 +215,114 @@ class _PDA(_Automaton):
                 {edges_str.strip()}
             }}\
         """
+
+    def _as_cfg(self) -> CFG:
+        """
+        Transform this PDA into an equivalent CFG.
+
+        Details of this transformation can be found in Sipser (3rd ed., 2013),
+        Lemma 2.27. Note that this is somewhat different than the approach in
+        Hopcroft, Motwani, and Ullman (3rd ed., 2006), Section 6.3.2.
+        """
+        # Verify that existing PDA states can't conflict with upcoming changes.
+        conflict_states = [s for s in self._states if "cfg" in s]
+        if len(conflict_states) > 0:
+            raise DetailedError(
+                "Conflicting state names",
+                "The PDA -> CFG conversion modifies this PDA with new states "
+                + "containing the string 'cfg', but this may conflict with "
+                + f"existing states: {conflict_states}.",
+            )
+
+        # First, modify this PDA so it has a single accept state.
+        mod_accept_states: set[State] = {"q_cfg_final"}
+        mod_states = self._states | mod_accept_states
+        mod_transitions: dict[tuple[State, str, str], set[tuple[State, str]]] = (
+            defaultdict(set)
+        )
+        for accept_state in self._accept_states:
+            mod_transitions[(accept_state, self._epsilon, self._epsilon)] = {
+                ("q_cfg_final", self._epsilon)
+            }
+
+        # Next, ensure any computation empties its stack before accepting.
+        for stack_sym in self._stack_alphabet:
+            mod_transitions[("q_cfg_final", self._epsilon, stack_sym)] = {
+                ("q_cfg_final", self._epsilon)
+            }
+
+        # Finally, ensure that every transition either pushes a symbol onto the
+        # stack or pops a symbol from the stack, but not both simultaneously.
+        aux_state_ix = 0
+        dummy_sym = next(iter(self._stack_alphabet))
+        for (q_from, input_sym, pop_sym), to_pairs in self._transitions.items():
+            for q_to, push_sym in to_pairs:
+                # Replace q_from (read a, pop b, push c) -> q_to with:
+                # 1. q_from -- (read a, pop b, push nothing) -> q_new
+                # 2. q_new -- (read nothing, pop nothing, push c) -> q_to
+                if pop_sym != self._epsilon and push_sym != self._epsilon:
+                    mod_transitions[(q_from, input_sym, pop_sym)].add(
+                        (f"q_cfg_{aux_state_ix}", self._epsilon)
+                    )
+                    mod_transitions[
+                        (f"q_cfg_{aux_state_ix}", self._epsilon, self._epsilon)
+                    ].add((q_to, push_sym))
+                    aux_state_ix += 1
+                # Replace q_from (read a, pop nothing, push nothing) -> q_to with:
+                # 1. q_from -- (read a, pop nothing, push dummy) -> q_new
+                # 2. q_new -- (read nothing, pop dummy, push nothing) -> q_to
+                elif pop_sym == self._epsilon and push_sym == self._epsilon:
+                    mod_transitions[(q_from, input_sym, self._epsilon)].add(
+                        (f"q_cfg_{aux_state_ix}", dummy_sym)
+                    )
+                    mod_transitions[
+                        (f"q_cfg_{aux_state_ix}", self._epsilon, dummy_sym)
+                    ].add((q_to, self._epsilon))
+                    aux_state_ix += 1
+                else:
+                    mod_transitions[(q_from, input_sym, pop_sym)].add((q_to, push_sym))
+
+        # The equivalent CFG's variables are PDA states pairs (p,q). Variable
+        # (p,q) will generate all strings that take the PDA from state p to
+        # state q while leaving the stack at state q exactly as it was in state
+        # p. The CFG's start variable is thus (q_0,q_accept).
+        cfg_variables = {f"({p},{q})" for (p, q) in product(self._states, repeat=2)}
+        cfg_start_variable = f"({self._start_state},q_cfg_final)"
+
+        # There are three types of rules in the equivalent CFG:
+        # 1. For each state p in the PDA, (p,p) -> epsilon, i.e., it is always
+        #    possible to transition from a state to itself with no input.
+        # 2. For each triple of states p, q, r in the PDA, (p,q) -> (p,r)(r,q),
+        #    i.e., one way of transitioning from p to q with the same start/end
+        #    stack configuration is first transitioning from p to r with that
+        #    stack configuration and then transitioning from r to q.
+        # 3. For each (p, q, r, s) of PDA states, stack symbol u, and (possibly
+        #    empty) input symbols a and b, if delta(p, a, epsilon) contains
+        #    (r, u) and delta(s, b, u) contains (q, epsilon), (p,q) -> a(r,s)b.
+        #    That is, one way of transitioning from p to q with the same start/
+        #    end stack configuration is to (1) transition from p to r, reading
+        #    a and pushing u; (2) transition from r to s with the same start/
+        #    end stack configuration, and finally (3) transition from s to q,
+        #    reading b and popping u.
+        cfg_rules: dict[str, set[tuple[str, ...]]] = defaultdict(set)
+        for p in mod_states:
+            cfg_rules[f"({p},{p})"].add(tuple(self._epsilon))
+            for q, r in product(mod_states, repeat=2):
+                cfg_rules[f"({p},{q})"].add((f"({p},{r})", f"({r},{q})"))
+                for s, u in product(mod_states, self._stack_alphabet):
+                    for a, b in product(
+                        self._input_alphabet | {self._epsilon}, repeat=2
+                    ):
+                        if (r, u) in mod_transitions[(p, a, self._epsilon)] and (
+                            q,
+                            self._epsilon,
+                        ) in mod_transitions[(s, b, u)]:
+                            cfg_rules[f"({p},{q})"].add((a, f"({r},{s})", b))
+
+        return CFG(
+            V=cfg_variables,
+            Sigma=self._input_alphabet,
+            R=cfg_rules,
+            S=cfg_start_variable,
+            epsilon=self._epsilon,
+        )
